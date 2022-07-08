813b00993935600114f0cc09b650a50743eadcf9
hzsn-csh <hzsn-csh@qq.com>
2022-07-08 14:41:20 +0800

    添加base

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ import {
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3333 },
+  base:'/sn-tools/',
   resolve: {
     alias: {
       "@": resolve(__dirname, 'src'), // 路径别名
