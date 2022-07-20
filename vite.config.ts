97b49e9b7a340a3668fc72c427fc4d5f2c75cfdd
hzsn-csh <hzsn-csh@qq.com>
2022-07-20 10:55:53 +0800

    添加构建扩展命令

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ import {
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 3333 },
-  base: '/sn-tools/',
   resolve: {
     alias: {
       "@": resolve(__dirname, 'src'), // 路径别名
