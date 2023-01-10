3adf031ba6321b10963c91417209a1cd4850b7bf
hzsn-csh <hzsn-csh@qq.com>
2023-01-10 15:53:59 +0800

    重构代码

--- src/page/ftms/composable/excel-util.ts
+++ src/page/ftms/composable/excel-util.ts
@@ -5,6 +5,7 @@
 
 import { read, utils, WorkBook, WorkSheet, write, WritingOptions } from "xlsx";
 import { KeyItemType, SheetItemType } from "../data";
+import { downloadFile } from "../util";
 
 function sheet2blob(
     sheetObject: {
@@ -40,24 +41,6 @@ function sheet2blob(
     return blob;
 }
 
-function openDownloadDialog(blob: Blob, saveName: string) {
-    let url = "";
-    if (typeof blob == 'object' && blob instanceof Blob) {
-        url = URL.createObjectURL(blob); // 创建blob地址
-    }
-    var aLink = document.createElement('a');
-    aLink.href = url;
-    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
-    var event;
-    if (window.MouseEvent) event = new MouseEvent('click');
-    else {
-        event = document.createEvent('MouseEvents');
-        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    }
-    aLink.dispatchEvent(event);
-    aLink.remove();
-}
-
 export const exportToXlsx = (
     dataObject: {
         [key: string]: {
@@ -78,7 +61,7 @@ export const exportToXlsx = (
         }
     }
 
-    openDownloadDialog(sheet2blob(sheetObject), getSaveName(Object.keys(sheetObject), fileName));
+    downloadFile(sheet2blob(sheetObject), getSaveName(Object.keys(sheetObject), fileName));
 }
 
 function getSaveName(sheetNames: string[], fileName: string) {
--- src/page/ftms/util.ts
+++ src/page/ftms/util.ts
@@ -11,6 +11,24 @@ export function prefix0(num: number) {
     return num < 10 && "0" + num || num;
 }
 
+export function downloadFile(blob: Blob, saveName: string) {
+    let url = "";
+    if (typeof blob == 'object' && blob instanceof Blob) {
+        url = URL.createObjectURL(blob); // 创建blob地址
+    }
+    var aLink = document.createElement('a');
+    aLink.href = url;
+    aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
+    var event;
+    if (window.MouseEvent) event = new MouseEvent('click');
+    else {
+        event = document.createEvent('MouseEvents');
+        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+    aLink.dispatchEvent(event);
+    aLink.remove();
+}
+
 /**
  * 线性扫描
  * @param weights 
