66452db3276df5c8064f4d5ab4ebc2962fa63c6e
hzsn-csh <hzsn-csh@qq.com>
2023-01-11 20:29:45 +0800

    更新version

---
+++ update.version.ts
@@ -0,0 +1,29 @@
+
+import { readFile, writeFile } from "fs/promises";
+
+function setVersion(version: string) {
+    let vNumList = version.split(".").map(item => Number(item));
+    if (vNumList[2] >= 9) {
+        vNumList[2] = 0;
+        vNumList[1] += 1;
+
+        if (vNumList[1] > 9) {
+            vNumList[1] = 0;
+            vNumList[0] += 1;
+        }
+    } else {
+        vNumList[2] += 1
+    }
+
+    return vNumList.join(".");
+}
+
+export default async () => {
+    let filePath = './package.json';
+    console.log("update version...");
+    let packageText = await readFile(filePath, { encoding: "utf8" });
+    let packageJson = JSON.parse(packageText);
+    packageJson.version = setVersion(packageJson.version);
+    await writeFile(filePath, JSON.parse(packageJson), { encoding: "utf8" });
+    console.log(`package version is updated:${packageJson.version}`);
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfigExport } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
@@ -11,87 +11,90 @@ import {
 } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: { port: 3333, host: "0.0.0.0" },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"), // 路径别名
-    },
-    extensions: [".js", ".json", ".ts"], // 使用路径别名时想要省略的后缀名，可以自己 增减
-  },
-  plugins: [
-    vue(),
-    viteCompression(),
-    createStyleImportPlugin({
-      resolves: [ElementPlusResolve()],
-      libs: [
-        {
-          libraryName: "element-plus",
-          esModule: true,
-          resolveStyle: (name: string) => {
-            return `element-plus/theme-chalk/${name}.css`;
-          },
-        },
-      ],
-    }),
-    AutoImport({
-      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
-      dirs: ["./src/components/*"],
-      dts: "./src/auto-imports.d.ts",
-      imports: ["vue", "vue-router", "pinia"],
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  build: {
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            const arr = id.toString().split('node_modules/')[1].split('/')
-            switch (arr[0]) {
-              case '@vue':
-              case 'axios':
-              case 'pinia':
-              case '@element-plus':
-              case 'element-plus':
-              case 'xlsx':
-                return '_' + arr[0]
-                break
-              default:
-                return '__vendor'
-                break
-            }
-          }
-        },
-        chunkFileNames: 'assets/js1/[name]-[hash].js',
-        entryFileNames: 'assets/js2/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
-      }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
+export default defineConfig(async ({ command, mode }) => {
+  let config: UserConfigExport = {
+    server: { port: 3333, host: "0.0.0.0" },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"), // 路径别名
       },
+      extensions: [".js", ".json", ".ts"], // 使用路径别名时想要省略的后缀名，可以自己 增减
     },
-    postcss: {
-      plugins: [
-        {
-          postcssPlugin: "internal:charset-removal",
-          AtRule: {
-            charset: (atRule) => {
-              if (atRule.name === "charset") {
-                atRule.remove();
-              }
+    plugins: [
+      vue(),
+      viteCompression(),
+      createStyleImportPlugin({
+        resolves: [ElementPlusResolve()],
+        libs: [
+          {
+            libraryName: "element-plus",
+            esModule: true,
+            resolveStyle: (name: string) => {
+              return `element-plus/theme-chalk/${name}.css`;
             },
           },
-        },
-      ],
+        ],
+      }),
+      AutoImport({
+        include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+        dirs: ["./src/components/*"],
+        dts: "./src/auto-imports.d.ts",
+        imports: ["vue", "vue-router", "pinia"],
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    build: {
+      minify: "terser",
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              const arr = id.toString().split('node_modules/')[1].split('/')
+              switch (arr[0]) {
+                case '@vue':
+                case 'axios':
+                case 'pinia':
+                case '@element-plus':
+                case 'element-plus':
+                case 'xlsx':
+                  return '_' + arr[0]
+                  break
+                default:
+                  return '__vendor'
+                  break
+              }
+            }
+          },
+          chunkFileNames: 'assets/js1/[name]-[hash].js',
+          entryFileNames: 'assets/js2/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+        }
+      }
     },
-  },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          charset: false,
+        },
+      },
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: "internal:charset-removal",
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === "charset") {
+                  atRule.remove();
+                }
+              },
+            },
+          },
+        ],
+      },
+    },
+  }
+  return config;
 });
