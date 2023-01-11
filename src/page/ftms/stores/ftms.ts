8f899cb3a7aebe0a85e85799b7a5ad420bae58ba
hzsn-csh <hzsn-csh@qq.com>
2023-01-11 13:41:08 +0800

    fix:更改时间

--- src/page/ftms/stores/ftms.ts
+++ src/page/ftms/stores/ftms.ts
@@ -4,9 +4,9 @@ import { SeriesKeyType, SeriesType } from "../data";
 export const useFtmsStore = defineStore("ftmsStore", () => {
 
     let toDay = new Date;
-    const dateTime: [Date, Date] = reactive([
+    const dateTime = ref([
         new Date(toDay.getFullYear(), toDay.getMonth(), toDay.getDate() - 1, 18),
-        new Date(toDay.getFullYear(), toDay.getMonth(), toDay.getDate(), 17, 59, 59)
+        new Date(toDay.getFullYear(), toDay.getMonth(), toDay.getDate(), 17, 30, 0)
     ]);
 
     const uploadTypeRadioKeyList = reactive(fileDataType);
