5f1b35a11e927ed3ba1ef919c4f260367e228e1e
hzsn <hzsn-csh@qq.com>
2023-03-03 20:52:34 +0800

    feat:添加代码复制

--- src/page/track/component/HighLightCode.vue
+++ src/page/track/component/HighLightCode.vue
@@ -1,5 +1,5 @@
 <template>
-    <highlightjs autodetect :code="jsonCode" />
+    <highlightjs id="highlightjs" autodetect :code="jsonCode" data-clipboard-target="#highlightjs" />
 </template>
 <script setup lang="ts">
 
@@ -7,9 +7,11 @@ import useTrack from "../composable/useTrack";
 import 'highlight.js/lib/common';
 import "highlight.js/styles/atom-one-dark.css"
 import hljsVuePlugin from "@highlightjs/vue-plugin";
+import ClipboardJS from "clipboard";
 
 const highlightjs = hljsVuePlugin.component;
 const { jsonCode } = useTrack();
+onMounted(() => { new ClipboardJS("#highlightjs"); });
 </script>
 <style>
 pre>code {
