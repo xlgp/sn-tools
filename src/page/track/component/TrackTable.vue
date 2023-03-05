4919deb9b1c32800d5e73cddfe44385d45bf832a
hzsn-csh <hzsn-csh@qq.com>
2023-03-05 14:25:46 +0800

    fix:初始化信息内容

--- src/page/track/component/TrackTable.vue
+++ src/page/track/component/TrackTable.vue
@@ -2,7 +2,11 @@
   <el-table :data="dataList" stripe>
     <el-table-column :prop="colunms.status" :label="colunms.status" width="120">
       <template #default="scope">
-        <el-select v-model="scope.row.status" placeholder="状态">
+        <el-select
+          v-model="scope.row.status"
+          placeholder="状态"
+          @change="((val:TrackStatusKey)=>changeStatusHandler(val, scope.row))"
+        >
           <el-option
             v-for="item in status_list"
             :key="item.code"
@@ -24,11 +28,11 @@
     </el-table-column>
     <el-table-column :prop="colunms.station" :label="colunms.station" width="150"
       ><template #default="scope"
-        ><el-input v-model="scope.row.station" placeholder="地点" /> </template
+        ><el-input v-model="scope.row.station" placeholder="地点" clearable /> </template
     ></el-table-column>
     <el-table-column :prop="colunms.info" :label="colunms.info">
       <template #default="scope">
-        <el-input v-model="scope.row.info" placeholder="更新信息" />
+        <el-input v-model="scope.row.info" placeholder="更新信息" clearable />
       </template>
     </el-table-column>
     <el-table-column fixed="right" label="操作" width="80">
@@ -42,15 +46,18 @@
 </template>
 
 <script setup lang="ts">
-import { track_status, colunms } from "../contants";
+import { track_status, colunms, TrackStatusKey } from "../contants";
 import { useTrackStore } from "../stores/track";
 import useTrack from "../composable/useTrack";
+import { TrackItemType } from "../data";
 
 const { dataList } = useTrackStore();
 const { deleteItem } = useTrack();
 const status_list = Object.values(track_status);
 
-const changeStatus = (value: string | number | boolean) => {
-  throw new Error("此处需要实现");
+const changeStatusHandler = (value: TrackStatusKey, item: TrackItemType) => {
+  if (!item.info) {
+    item.info = track_status[value].text;
+  }
 };
 </script>
--- src/page/track/contants.ts
+++ src/page/track/contants.ts
@@ -1,16 +1,28 @@
 import { TrackStatusType } from "./data";
 
+export enum TrackStatusEnum {
+    PACKAGE = 0,
+    //运输中
+    TRACKING = 1,
+    //派送中
+    DELIVERY = 2,
+    //已签收
+    SIGNED = 3
+}
+
+export type TrackStatusKey = TrackStatusEnum.PACKAGE | TrackStatusEnum.TRACKING | TrackStatusEnum.DELIVERY | TrackStatusEnum.SIGNED;
+
 export const track_status: {
-    [key: string]: TrackStatusType;
+    [key: number]: TrackStatusType;
 } = {
     //揽收
-    PACKAGE: { code: 0, text: "揽收" },
+    [TrackStatusEnum.PACKAGE]: { code: 0, text: "揽收" },
     //运输中
-    TRACKING: { code: 1, text: "运输中" },
+    [TrackStatusEnum.TRACKING]: { code: 1, text: "运输中" },
     //派送中
-    DELIVERY: { code: 2, text: "派送中" },
+    [TrackStatusEnum.DELIVERY]: { code: 2, text: "派送中" },
     //已签收
-    SIGNED: { code: 3, text: "签收" },
+    [TrackStatusEnum.SIGNED]: { code: 3, text: "签收" },
 };
 
 export const colunms = {
